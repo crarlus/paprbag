415f83624b475b0e2ad8ffb831fefd29b9593804
unknown <denekec@NU-NB-06640.rki.local>
2016-01-14 20:38:14 +0100

    disable default warning

--- R/GetAAindexStats.R
+++ R/GetAAindexStats.R
@@ -12,7 +12,7 @@ GetAAindexStats <- function(AAfreq,AAindex_Selection){
   data(AAindexData)
 
   # Default selection of indices
-  if(is.null(AAindex_Selection)) {AAindex_Selection <- c("BROC820101", "BUNA790103", "CHOP780207", "FAUJ880105", "FINA910103", "GEIM800103", "GEIM800105", "ISOY800107", "KHAG800101", "LEWP710101","MAXF760103",                                                         "OOBM850104", "PALJ810111", "PRAM820103", "QIAN880102", "QIAN880114", "QIAN880123", "QIAN880137", "RACS770103", "RACS820103", "RICJ880101", "RICJ880117",                                                         "ROBB760107", "TANS770106", "TANS770108", "VASM830101", "WERD780104", "AURR980102", "AURR980116", "AURR980118", "FUKS010109", "SUYM030101"); warning("No Amino acid inidices selected. Use default indices") }
+  if(is.null(AAindex_Selection)) {AAindex_Selection <- c("BROC820101", "BUNA790103", "CHOP780207", "FAUJ880105", "FINA910103", "GEIM800103", "GEIM800105", "ISOY800107", "KHAG800101", "LEWP710101","MAXF760103",                                                         "OOBM850104", "PALJ810111", "PRAM820103", "QIAN880102", "QIAN880114", "QIAN880123", "QIAN880137", "RACS770103", "RACS820103", "RICJ880101", "RICJ880117",                                                         "ROBB760107", "TANS770106", "TANS770108", "VASM830101", "WERD780104", "AURR980102", "AURR980116", "AURR980118", "FUKS010109", "SUYM030101"); }
 
   AAindex_rows <- match(AAindex_Selection,AAindexData$AccNo)
   if(any(is.na(AAindex_rows)) ) {
