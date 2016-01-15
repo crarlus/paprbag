607e2d6c5aa0caa06f8928f8439a879895f2565b
unknown <denekec@NU-NB-06640.rki.local>
2016-01-15 15:57:28 +0100

    Label data with human P and NP only

--- R/data.R
+++ R/data.R
@@ -71,7 +71,7 @@
 
 #' Pathogenicity labels
 #' @details
-#' The set of labels for 4286 bacterial strains (481 non-pathogens and 3805 pathogens)
+#' The set of labels for 4286 bacterial strains (181 non-pathogens and 2838 pathogens)
 #' TRUE means pathogenic and FALSE means non-pathogenic
 #' The species' bioproject accession is obtained by names(Labels)
 #' @references
